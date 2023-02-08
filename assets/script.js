1fb51d3e862ac88baebff699f9fb272ad69fa1f5
LL8719 <arsgatslr_87@yahoo.com>
2023-02-07 20:54:56 -0700

    created button from user selection and added copy feature to the button

--- assets/script.js
+++ assets/script.js
@@ -14,10 +14,12 @@ function findJoke(joke) {
 //Displays Joke
 function displayJoke(data) {
 	var newJoke = $('<h5>').text(data.joke);
+	newJoke.attr('id', 'mjoke');
 	$('.joke').append(newJoke);
 }
 
 var motivateUrl = 'https://api.goprogram.ai/inspiration';
+console.log(motivateUrl);
 //Finds Motivational quote
 function motivate(quote) {
 	fetch(motivateUrl)
@@ -25,12 +27,15 @@ function motivate(quote) {
 			return response.json();
 		})
 		.then((data) => {
+			console.log(data);
 			displayMotivate(data);
 		});
 }
 //Displays Motivational quote
 function displayMotivate(data) {
 	var newQuote = $('<h5>').text(data.quote);
+	newQuote.attr('id', 'mquote');
+
 	$('.random_quote').append(newQuote);
 }
 
@@ -48,6 +53,42 @@ function saveSelection() {
 $('.save_button').on('click', function (e) {
 	e.preventDefault();
 	saveSelection();
+	makeButton();
 });
 
+// Make button
+function makeButton() {
+	$('.search_history').empty();
+
+	var savedText = $('<button>').text(
+		$('.joke').text() + ' ' + $('.random_quote').text()
+	);
+	savedText.addClass('btn  btn-primary');
+	savedText.attr('id', 'savedBtnText');
+	$('.search_history').append(savedText);
+	savedText.on('click', (event) => {
+		event.preventDefault();
+		copyText();
+	});
+}
+//Copy text from button
+function copyText() {
+	// Get the text field
+	var jokeMotiv = $('#savedBtnText').text();
+	var copyText = jokeMotiv;
+
+	// Copy the text inside the text field
+	navigator.clipboard.writeText(copyText);
+
+	// Alert the copied text
+	alert('Copied the text: ' + copyText);
+}
+// Skip button
+$('.skip_button').on('click', function (event) {
+	event.preventDefault();
+	$('.joke').empty();
+	$('.random_quote').empty();
+
+	findJoke();
+});
 findJoke();
